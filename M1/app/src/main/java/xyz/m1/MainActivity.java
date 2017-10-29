9f75e1b94b4806f2af32b1acf74b40422c21fc9a
sid <siddhant.gupta021295@gmail.com>
2017-10-29 11:49:20 +0530

    frist commit

---
+++ M1/app/src/main/java/xyz/m1/MainActivity.java
@@ -0,0 +1,32 @@
+package xyz.m1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button;
+        final EditText editText;
+        button = (Button)findViewById(R.id.Button1);
+        editText = (EditText)findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String pid = editText.getText().toString();
+                Intent intent= new Intent(MainActivity.this,SendingData.class);
+                intent.putExtra("pid",pid);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+}
